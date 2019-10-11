2eb0f9c11500ea34ec375344f49cd6fbf30135b6
sudo-gera <gera.tatarinov.999@gmail.com>
2019-10-11 11:51:58 +0300

    api

--- draw.py
+++ draw.py
@@ -1,12 +1,28 @@
 import os
 ls=0
-screen=[]
-while 1:
+def enable(q):
+ a=open(q)
+ map1=a.read()
+ a.close()
+ map1=map1.split('\n')
+ map1=[list(w) for w in map1]
+ return map1
+map=[enable('1.map'),enable('2.map')]
+def place(x,y,q,nmap):
+ global map
+ for w in range(len(q)):
+  for e in range(len(q[w])):
+   map[nmap][x+w][y+e]=q[w][e]
+table=enable('table')
+for w in range(5):
+ for e in range(2):
+  place(10+w*15,10+e*15,table,0)
+def button():
  nls=int(os.popen('ls -l key.a').read().split()[4])
- z=open('key.a')
- z.read(ls)
- fs+=z.read(nls-ls)
- z.close()
+ a=open('key.a')
+ a.read(ls)
+ fs+=a.read(nls-ls)
+ a.close()
  ls=nls
 
  for w in screen:

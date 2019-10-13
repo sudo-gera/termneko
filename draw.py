0b1ea903ae294063a4a6411ad5586d6d4fd37585
sudo-gera <gera.tatarinov.999@gmail.com>
2019-10-13 20:34:35 +0300

    h

--- draw.py
+++ draw.py
@@ -1,5 +1,5 @@
 import os
-ls=0
+from os import get_terminal_size as gt
 def enable(q):
  a=open(q)
  map1=a.read()
@@ -7,28 +7,82 @@ def enable(q):
  map1=map1.split('\n')
  map1=[list(w) for w in map1]
  return map1
-maps=[enable('1.map'),enable('2.map')]
 def place(x,y,q,nmap):
- global maps
  for w in range(len(q)):
   for e in range(len(q[w])):
    maps[nmap][x+w][y+e]=q[w][e]
-table=enable('table')
-for w in range(5):
- for e in range(2):
-  place(10+w*15,10+e*15,table,0)
+def entity(x,y):
+ global emap
+ if y%2:
+  emap[y//2][x]='▄'
+ else:
+  emap[y//2][x]='▀'
 def button():
- nls=int(os.popen('ls -l key.a').read().split()[4])
+ #nls=int(os.popen('ls -l key.a').read().split()[4])
  a=open('key.a')
  global ls
  a.read(ls)
- fs=a.read(nls-ls)
+ fs=a.read()
+ ls+=len(fs)
  a.close()
- ls=nls
  return fs
+def draw():
+ global wx,wy,px,pyy,emap
+ wy=min(wy,py//2-4)
+ wx=min(wx,px-8)
+ wy=max(wy,py//2-gt()[1]+4)
+ wx=max(wx,px-gt()[0]+8)
+ tmp=emap[:]
+ tmp=tmp[max(wy,0):max(wy+gt()[1],0)]
+ tmp=[w[max(wx,0):max(wx+gt()[0],0)] for w in tmp]
+ tmp=[''.join(w) for w in tmp]
+ tmp=['\x1b['+str(1+w+max(0,-wy))+';'+str(max(0,-wx))+'H'+tmp[w] for w in range(len(tmp))]
+ tmp=''.join(tmp)
+ pr=tmp
+ print(pr,end='')
+def free(x,y,p):
+ global maps
+ try:
+  return maps[p][y//2][x]==' '
+ except:
+  return 0
+print('\n'*(gt()[1]-1),end='')
+ls=0
+maps=[enable('1.map'),enable('2.map')]
+table=enable('table')
+for w in range(8):
+ for e in range(2):
+  place(5+w*8,5+e*16,table,0)
 pmap=0
-px=5
-py=5
+px=55
+py=55
+wx=0
+wy=0
+but=''
 while 1:
- but=button()
- 
+ ww=''
+ but+=button()
+ if but:
+  ww=but[0]
+  but=but[1:]
+ if ww == 'p':
+  print('\x1b[0;0H',end='')
+  print(' '*gt()[0]*gt()[1],end='')
+  print('\x1b[0;0H',end='')
+  exit()
+ if ww == 'd':
+  if free(px+1,py,pmap):
+   px+=1
+ if ww == 'w':
+  if free(px,py-1,pmap):
+   py-=1
+ if ww == 'a':
+  if free(px-1,py,pmap):
+   px-=1
+ if ww == 's':
+  if free(px,py+1,pmap):
+   py+=1
+ emap=[w[:] for w in maps[pmap]]
+ entity(px,py)
+ draw()
+ print('\x1b[0;0H',ls,but)
--- map.py
+++ map.py
@@ -1,33 +1,37 @@
 for w in range(9608//256*256,(9608+256)//256*256):
  print(w,chr(w))
 a=open('2.map','w')
-a.write(chr(9556)+chr(9552)*64+chr(9574)+chr(9552)*64+chr(9559)+'\n')
+a.write(' '*133+'\n')
+a.write(' '+chr(9556)+chr(9552)*64+chr(9574)+chr(9552)*64+chr(9559)+' \n')
 for w in range(28):
- a.write(chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+'\n')
+ a.write(' '+chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+' \n')
 for w in range(4):
- a.write(chr(9553)+' '*64+' '+' '*64+chr(9553)+'\n')
-a.write(chr(9568)+chr(9552)*64+chr(9571)+' '*64+chr(9553)+'\n')
+ a.write(' '+chr(9553)+' '*64+' '+' '*64+chr(9553)+' \n')
+a.write(' '+chr(9568)+chr(9552)*64+chr(9571)+' '*64+chr(9553)+' \n')
 for w in range(4):
- a.write(chr(9553)+' '*64+' '+' '*64+chr(9553)+'\n')
+ a.write(' '+chr(9553)+' '*64+' '+' '*64+chr(9553)+' \n')
 for w in range(20):
- a.write(chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+'\n')
-a.write(chr(9553)+' '*64+chr(9568)+' '*8+chr(9552)*56+chr(9571)+'\n')
+ a.write(' '+chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+' \n')
+a.write(' '+chr(9553)+' '*64+chr(9568)+' '*8+chr(9552)*56+chr(9571)+' \n')
 for w in range(7):
- a.write(chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+'\n')
-a.write(chr(9562)+chr(9552)*64+chr(9577)+chr(9552)*64+chr(9565)+'\n')
+ a.write(' '+chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+' \n')
+a.write(' '+chr(9562)+chr(9552)*64+chr(9577)+chr(9552)*64+chr(9565)+' \n')
+a.write(' '*133+'\n')
 a.close()
 a=open('1.map','w')
-a.write(chr(9556)+chr(9552)*64+chr(9574)+chr(9552)*64+chr(9559)+'\n')
+a.write(' '*133+'\n')
+a.write(' '+chr(9556)+chr(9552)*64+chr(9574)+chr(9552)*64+chr(9559)+' \n')
 for w in range(23):
- a.write(chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+'\n')
+ a.write(' '+chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+' \n')
 for w in range(4):
- a.write(chr(9553)+' '*64+' '+' '*64+chr(9553)+'\n')
+ a.write(' '+chr(9553)+' '*64+' '+' '*64+chr(9553)+' \n')
 for w in range(30):
- a.write(chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+'\n')
-a.write(chr(9553)+' '*64+chr(9568)+' '*0+chr(9552)*64+chr(9571)+'\n')
+ a.write(' '+chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+' \n')
+a.write(' '+chr(9553)+' '*64+chr(9568)+' '*0+chr(9552)*64+chr(9571)+' \n')
 for w in range(4):
- a.write(chr(9553)+' '*64+chr(9553)*0+' '+' '*64+chr(9553)+'\n')
+ a.write(' '+chr(9553)+' '*64+chr(9553)*0+' '+' '*64+chr(9553)+' \n')
 for w in range(3):
- a.write(chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+'\n')
-a.write(chr(9562)+chr(9552)*64+chr(9577)+chr(9552)*64+chr(9565)+'\n')
+ a.write(' '+chr(9553)+' '*64+chr(9553)+' '*64+chr(9553)+' \n')
+a.write(' '+chr(9562)+chr(9552)*64+chr(9577)+chr(9552)*64+chr(9565)+' \n')
+a.write(' '*133+'\n')
 a.close()
